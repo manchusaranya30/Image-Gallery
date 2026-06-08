1fac48cc9ab5e2eef3b20243538e8e197ad918ba
manchusaranya30 <manchusaranya18@gmail.com>
2026-06-08 23:30:59 +0530

    Add image filtering and search functionality

--- script.js
+++ script.js
@@ -1,35 +1,81 @@
-const galleryImages = document.querySelectorAll(".gallery img");
+const cards = document.querySelectorAll(".image-card");
 
-const lightbox = document.getElementById("lightbox");
+function filterImages(category){
 
-const lightboxImg = document.getElementById("lightbox-img");
+cards.forEach(card=>{
 
-const closeBtn = document.querySelector(".close");
-
-galleryImages.forEach(image => {
-
-    image.addEventListener("click", () => {
-
-        lightbox.style.display = "flex";
-
-        lightboxImg.src = image.src;
-
-    });
+if(category==="all"){
+card.style.display="block";
+}
+else{
+card.style.display=
+card.classList.contains(category)
+? "block"
+: "none";
+}
 
 });
 
-closeBtn.addEventListener("click", () => {
+}
 
-    lightbox.style.display = "none";
+function searchImages(){
+
+const input=document
+.getElementById("searchInput")
+.value
+.toLowerCase();
+
+cards.forEach(card=>{
+
+const text=card.textContent.toLowerCase();
+
+if(text.includes(input)){
+card.style.display="block";
+}
+else{
+card.style.display="none";
+}
 
 });
 
-lightbox.addEventListener("click", (e) => {
+}
 
-    if(e.target === lightbox){
+const images=document.querySelectorAll(".image-card img");
 
-        lightbox.style.display = "none";
+const lightbox=document.getElementById("lightbox");
+const lightboxImg=document.getElementById("lightbox-img");
+const close=document.querySelector(".close");
 
-    }
+images.forEach(img=>{
+
+img.addEventListener("click",()=>{
+
+lightbox.style.display="flex";
+lightboxImg.src=img.src;
 
 });
+
+});
+
+close.addEventListener("click",()=>{
+
+lightbox.style.display="none";
+
+});
+
+lightbox.addEventListener("click",(e)=>{
+
+if(e.target===lightbox){
+lightbox.style.display="none";
+}
+
+});
+
+document.getElementById("topBtn").onclick=()=>{
+
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+
+};
