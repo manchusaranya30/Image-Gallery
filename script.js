3a4083890c7b6c616ba0ded03cf064a3ee139cdc
manchusaranya30 <manchusaranya18@gmail.com>
2026-06-08 21:11:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const galleryImages = document.querySelectorAll(".gallery img");
+
+const lightbox = document.getElementById("lightbox");
+
+const lightboxImg = document.getElementById("lightbox-img");
+
+const closeBtn = document.querySelector(".close");
+
+galleryImages.forEach(image => {
+
+    image.addEventListener("click", () => {
+
+        lightbox.style.display = "flex";
+
+        lightboxImg.src = image.src;
+
+    });
+
+});
+
+closeBtn.addEventListener("click", () => {
+
+    lightbox.style.display = "none";
+
+});
+
+lightbox.addEventListener("click", (e) => {
+
+    if(e.target === lightbox){
+
+        lightbox.style.display = "none";
+
+    }
+
+});
